e3a52d0860eae72f8dfab7a1fe3b1ea20a4eca46
Thomas Battram <tb13101@bristol.ac.uk>
2020-03-26 12:21:07 +0000

    started script to combine data and clean it up

---
+++ in-house-ewas/scripts/combine_traits_and_covariates.R
@@ -0,0 +1,58 @@
+# ---------------------------------------------
+# Tidying traits and combining with covariates for alspac
+# ---------------------------------------------
+
+pkgs <- c("tidyverse")
+lapply(pkgs, require, character.only = TRUE)
+
+source("scripts/read_filepaths.R")
+
+read_filepaths("filepaths.sh")
+# 
+
+# ---------------------------------------------
+# load in data! 
+# ---------------------------------------------
+
+# aries ids file
+aries_ids <- read_tsv(paste0("data/alspac/", aries_ids_file))
+# pcs
+pcs <- read.table(paste0("data/alspac/", timepoints, "_pcs.eigenvec"), sep = " ", header = F, stringsAsFactors = F) 
+head(pcs)
+colnames(pcs) <- c("FID", "IID", paste0(rep("PC", times = 20), 1:20))
+pcs$ALN <- gsub("[A-Z]", "", pcs[["FID"]])
+pcs <- dplyr::select(pcs, -IID, -FID)
+pc_cols <- colnames(pcs)[colnames(pcs) != "ALN"]
+
+# samplesheet
+load(samplesheet_file)
+head(samplesheet)
+
+# phenotype file
+phen_dat <- read_tsv(paste0("data/alspac/phenotype_data_", timepoints, ".txt"))
+phen_cols <- colnames(phen_dat)
+# ---------------------------------------------
+# combine data! 
+# ---------------------------------------------
+
+all_dat <- samplesheet %>%
+	dplyr::filter(ALN %in% aries_ids$ALN & time_point == timepoints) %>%
+	left_join(pcs) %>%
+	mutate(aln = as.numeric(ALN)) %>%
+	left_join(phen_dat) %>%
+	dplyr::select(one_of(phen_cols), age, one_of(pc_cols))
+
+# ---------------------------------------------
+# check phenotype data 
+# ---------------------------------------------
+
+
+# ---------------------------------------------
+# make SVs here or separate script??
+# ---------------------------------------------
+
+
+# ---------------------------------------------
+# save it all
+# ---------------------------------------------
+
