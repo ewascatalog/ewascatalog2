c9e29e27f49d8d4314a45f7bac16b01078e721f3
Thomas Battram <tb13101@bristol.ac.uk>
2020-04-03 16:08:16 +0100

    updated functions so they now work

--- in-house-ewas/scripts/make_svs.R
+++ in-house-ewas/scripts/make_svs.R
@@ -16,7 +16,7 @@ impute_matrix <- function(x, FUN = function(x) rowMedians(x, na.rm = T)) {
 # function to add quotes for weird trait names
 addq <- function(x) paste0("`", x, "`")
 
-out_failed <- function(x) {
+out_failed <- function(x, out_path) {
 	sink(file = paste0(out_path, "sv_fails.txt"), append = TRUE, split = TRUE)
 	print(x)
 	sink()
@@ -24,11 +24,18 @@ out_failed <- function(x) {
 
 generate_svs <- function(trait, phen_data, meth_data, covariates, nsv, out_path) {
 	print(trait)
+	out_nam <- paste0(out_path, trait, ".txt")
+	if (file.exists(out_nam)) {
+		message(out_nam, " exists so moving on")
+		return(NULL)
+	}
 	phen <- phen_data %>%
 		dplyr::select(Sample_Name, one_of(trait, covariates)) %>%
 		.[complete.cases(.), ]
 	
 	mdat <- meth_data[, colnames(meth_data) %in% phen$Sample_Name]
+	phen <- phen %>%
+		dplyr::filter(Sample_Name %in% colnames(mdat))
 	# full model - with variables of interest 
 	fom <- as.formula(paste0("~", addq(trait), " + ",paste(covariates, collapse = " + ")))
 	mod <- model.matrix(fom, data = phen)
@@ -41,10 +48,11 @@ generate_svs <- function(trait, phen_data, meth_data, covariates, nsv, out_path)
 		svobj <- smartsva.cpp(mdat, mod, mod0, n.sv = nsv)
 		svs <- as.data.frame(svobj$sv, stringsAsFactors = F)
 		svs$Sample_Name <- phen$Sample_Name
-		head(svs)
+		# head(svs)
 		colnames(svs)[1:nsv] <- paste0("sv", 1:nsv)
 
-		write.table(svs, file = paste0(out_path, i, ".txt"), sep = "\t", quote = F, col.names = T, row.names = F)
+		write.table(svs, file = paste0(out_path, trait, ".txt"),
+					sep = "\t", quote = F, col.names = T, row.names = F)
 
-	}, error = function(e) {out_failed(i)})
+	}, error = function(e) {out_failed(trait, out_path)})
 }
