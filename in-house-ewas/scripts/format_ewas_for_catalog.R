8d974eee09636abc460daed07dabdfc188cb9dd4
Thomas Battram <tb13101@bristol.ac.uk>
2020-08-23 17:10:34 +0100

    updated so the script syncs with the new ewas script for inhouse data that looks to align that pipeline with the publication extraction and user upload pipelines

--- in-house-ewas/scripts/format_ewas_for_catalog.R
+++ in-house-ewas/scripts/format_ewas_for_catalog.R
@@ -2,6 +2,11 @@
 # Script for formatting EWAS results for the catalog
 # --------------------------------------------------------
 
+# Just format results so they're ready for pipeline:
+# 1. Bind all the meta files together for the studies file
+# 2. Loop through the studies and extract data at P<1e-4
+# 3. Tidy up studies file and output results into a "results/" directory
+
 pkgs <- c("tidyverse", "readxl", "openxlsx")
 lapply(pkgs, require, character.only = TRUE)
 
@@ -19,48 +24,61 @@ extra_cohort_info <- args[2]
 
 # res_dir <- paste0(local_rdsf_dir, "data/", cohort, "/results/")
 res_dir <- file.path("results", cohort)
-
 if (!file.exists(res_dir)) stop("File path not found!")
 
-# cpg annotations
-cpg_anno_file <- "cpg_annotation.txt"
-if (!file.exists(cpg_anno_file)) stop("move cpg annotation file from rdsf")
-cpg_anno <- read_tsv(cpg_anno_file)
-
 # file paths 
-raw_path <- file.path(res_dir, "raw/")
-derived_path <- file.path(res_dir, "derived/")
+raw_path <- file.path(res_dir, "raw")
+derived_path <- file.path(res_dir, "derived")
 
 extra_cohort_dirs <- list.files(raw_path)
 
-dir <- extra_cohort_dirs[1]
-sub_res <- map_dfr(extra_cohort_dirs, function(dir) {
-	print(dir)
-	raw_res_path <- file.path(raw_path, dir, "full_stats/")
-	raw_res_files <- list.files(raw_res_path)
-	dir_sub_res <- map_dfr(raw_res_files, function(x) {
-		full_out_nam <- gsub(".txt", "_full_stats.txt", x)
-		df <- read_tsv(paste0(raw_res_path, x)) %>%
-			rename(CpG = probeID, Beta = estimate, SE = se, P = p.value) %>%
-			left_join(cpg_anno) %>%
-			dplyr::select(CpG, Location, Chr, Pos, Gene, Type, Beta, SE, P, Details, StudyID)
-		
-		# write it out to the rdsf
-		derived_res_nam <- file.path(derived_path, dir, full_out_nam)
-		write.table(df, file = derived_res_nam, 
-				col.names = T, row.names = F, quote = F, sep = "\t")
-
-		# now just take values of p<1x10-4
-		sub_df <- df %>%
-			dplyr::filter(P < 1e-4) %>%
-			mutate(Beta = round(Beta, 6), 
-				   SE = round(SE, 6), 
-				   P = signif(P, 3))
-		return(sub_df)
+# bind studies files together
+di=extra_cohort_dirs[1]
+meta_dat <- map_dfr(extra_cohort_dirs, function(di) {
+	meta_files <- grep("catalog_meta_data", list.files(file.path(raw_path, di)), value=T)
+	if (length(meta_files) == 0) return(NULL)
+	meta_out <- map_dfr(meta_files, function(f) {
+		out <- read_tsv(file.path(raw_path, di, f))
+		system(paste0("rm ", raw_path, "/", di, "/", f))
+		return(out)
 	})
-	return(dir_sub_res)
+	return(meta_out)
 })
 
-results_nam <- paste0(derived_path, "results.txt")
-write.table(sub_res, file = results_nam, 
-			col.names = T, row.names = F, quote = F, sep = "\t")
+# check which ewas have not run
+old_meta_dat <- map_dfr(extra_cohort_dirs, function(di) {
+	cohort_data_path <- file.path(cohort, di)
+	pheno_meta_file <- file.path("data", cohort_data_path, "phenotype_metadata.txt")
+	pheno_meta <- read_tsv(pheno_meta_file)
+	return(pheno_meta)
+})
+
+failed_phens <- old_meta_dat %>%
+	dplyr::filter(!phen %in% meta_dat$phen) %>%
+	pull(phen)
+
+studies_columns <- c("Author", "Consortium", "PMID", "Date", "Trait", 
+					 "EFO", "Trait_units", "dnam_in_model", "dnam_units", 
+					 "Analysis", "Source", "Covariates", "Methylation_Array", 
+					 "Tissue", "Further_Details", "N", "N_Cohorts", "Age", "Sex",
+					 "Ethnicity", "Results_file")
+x=1
+studies <- map_dfr(1:nrow(meta_dat), function(x) {
+	print(x)
+	df <- meta_dat[x, ]
+	# read in full stats and get P<1e-4
+	derived_dat <- read_tsv(df$full_stats_file) %>%
+		dplyr::filter(p.value < 1e-4) %>%
+		rename(CpG = probeID, Beta = estimate, SE = se, P = p.value)
+	# if no results return null
+	if (nrow(derived_dat) == 0) return(NULL)
+	# write out results to already determined results file
+	write.csv(derived_dat, file = file.path(derived_path, "results", df$Results_file), 
+			  row.names = F, quote = F)
+	# extract data for studies file
+	studies_out <- df %>%
+		dplyr::select(one_of(studies_columns))
+	return(studies_out)
+})
+
+write.xlsx(studies, file = file.path(derived_path, "studies.xlsx"))
